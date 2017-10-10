0444e0304f645a5a0604dfbf3250f4155af20d13
Yohan Lascombe <ylascombe@octo.com>
2017-10-11 01:20:17 +0200

    Improve test when http server connection

--- server/server_test.go
+++ server/server_test.go
@@ -22,24 +22,20 @@ func TestCreateListenServerWhenNoConnection(t *testing.T) {
 	assert.Equal(t, -1, res.Status)
 }
 
-
 func TestCreateListenServerWhenConnection(t *testing.T) {
 	// arrange
 	channel := make(chan models.MockServerResult,1)
-	timeout := 20
+	timeout := 5
 	port := 9091
 	url := fmt.Sprintf("http://localhost:%v/", port)
-	fmt.Println(url)
-	// act
-	CreateListenServer(port, timeout, channel)
 
-	resp, err := http.Get(url)
+	// act
+	go CreateListenServer(port, timeout, channel)
+
+	go http.Get(url)
 
 	// assert
-	assert.Nil(t, err)
-
 	res := <- channel
 	assert.NotNil(t, res)
 	assert.Equal(t, 0, res.Status)
-	assert.Equal(t, "", resp)
 }
