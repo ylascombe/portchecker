fa2ae1baa5f1a9f12478b7657bdb9b913cd69efd
Yohan Lascombe <ylascombe@octo.com>
2017-10-25 23:40:13 +0200

    Add test to generae test dataset

--- database/driver_test.go
+++ database/driver_test.go
@@ -48,3 +48,133 @@ func TestTearDown(t *testing.T) {
 	//res := db.Exec("delete from checkagent where email = ?", email).Error
 	//assert.Nil(t, res)
 }
+
+
+func TestDataset(t *testing.T) {
+
+
+	db := NewDBDriver()
+	defer db.Close()
+
+	// LOAD BALANCERS
+	checkAgentInFluxLB1 := []db_models.CheckAgentInFlux{}
+
+	checkAgentInFluxLB1 = append(checkAgentInFluxLB1, db_models.CheckAgentInFlux{
+		Requested: true,
+		Port: 80,
+		From: "*",
+	})
+
+	checkAgentInFluxLB1 = append(checkAgentInFluxLB1, db_models.CheckAgentInFlux{
+		Requested: true,
+		Port: 443,
+		From: "*",
+	})
+
+	checkAgentOutFluxLB1 := []db_models.CheckAgentOutFlux{}
+
+	checkAgentOutFluxLB1 = append(checkAgentOutFluxLB1, db_models.CheckAgentOutFlux{
+		Status: true,
+		Port: 8080,
+		To: "webserver1",
+	})
+
+	checkAgentOutFluxLB1 = append(checkAgentOutFluxLB1, db_models.CheckAgentOutFlux{
+		Status: true,
+		Port: 8080,
+		To: "webserver2",
+	})
+
+	lb1 := db_models.CheckAgent{
+		AnalysisId: 1,
+		Hostname:   "lb1",
+		InFlux:     checkAgentInFluxLB1,
+		OutFlux:    checkAgentOutFluxLB1,
+	}
+
+	lb2 := db_models.CheckAgent{
+		AnalysisId: 1,
+		Hostname:   "lb2",
+		InFlux:     checkAgentInFluxLB1,
+		OutFlux:    checkAgentOutFluxLB1,
+	}
+
+	db.Create(&lb1)
+	db.Create(&lb2)
+
+
+	// WEBSERVERS
+	checkAgentInFluxWebServer := []db_models.CheckAgentInFlux{}
+
+	checkAgentInFluxWebServer = append(checkAgentInFluxWebServer, db_models.CheckAgentInFlux{
+		Requested: true,
+		Port: 8080,
+		From: "lb1",
+	})
+
+	checkAgentInFluxWebServer = append(checkAgentInFluxWebServer, db_models.CheckAgentInFlux{
+		Requested: true,
+		Port: 8080,
+		From: "lb2",
+	})
+
+	checkAgentOutFluxWebserver := []db_models.CheckAgentOutFlux{}
+
+	checkAgentOutFluxWebserver = append(checkAgentOutFluxWebserver, db_models.CheckAgentOutFlux{
+		Status: true,
+		Port: 3306,
+		To: "db-master",
+	})
+	webserver1 := db_models.CheckAgent{
+		AnalysisId: 1,
+		Hostname:   "webserver1",
+		InFlux:     checkAgentInFluxWebServer,
+		OutFlux:    checkAgentOutFluxWebserver,
+	}
+
+	webserver2 := db_models.CheckAgent{
+		AnalysisId: 1,
+		Hostname:   "webserver2",
+		InFlux:     checkAgentInFluxWebServer,
+		OutFlux:    checkAgentOutFluxWebserver,
+	}
+
+	// Migrate the schema
+	db.Create(&webserver1)
+	db.Create(&webserver2)
+
+
+	// DATABASE
+	checkAgentInFluxDb := []db_models.CheckAgentInFlux{}
+
+	checkAgentInFluxDb = append(checkAgentInFluxDb, db_models.CheckAgentInFlux{
+		Requested: true,
+		Port: 3306,
+		From: "webserver1",
+	})
+
+	checkAgentInFluxWebServer = append(checkAgentInFluxWebServer, db_models.CheckAgentInFlux{
+		Requested: true,
+		Port: 3306,
+		From: "webserver2",
+	})
+
+	checkAgentOutFluxDb := []db_models.CheckAgentOutFlux{}
+
+	checkAgentOutFluxDb = append(checkAgentOutFluxDb, db_models.CheckAgentOutFlux{
+		Status: true,
+		Port: 9090,
+		To: "prometheus",
+	})
+
+	checkAgentDb := db_models.CheckAgent{
+		AnalysisId: 1,
+		Hostname:   "db-master",
+		InFlux:     checkAgentInFluxDb,
+		OutFlux:    checkAgentOutFluxDb,
+	}
+
+	db.Create(&checkAgentDb)
+
+
+}
